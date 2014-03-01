e55694882ea59db705579056c0083453b8c27201
Richard Hull <rm_hull@yahoo.co.uk>
2014-03-01 17:38:11 +0000

    Don't do stemming if searching with position and sense

--- src/clj_wordnet/core.clj
+++ src/clj_wordnet/core.clj
@@ -39,15 +39,17 @@
 (defn- stem [^IDictionary dict lemma part-of-speech]
   (.findStems (WordnetStemmer. dict) lemma (coerce/pos part-of-speech)))
 
-(defn- word-ids [^IDictionary dict lemma part-of-speech]
-  (let [pos (coerce/pos part-of-speech)
-        stems (stem dict lemma part-of-speech)]
-    (mapcat (memfn ^IIndexWord getWordIDs)
-            (for [stem stems
-                  :let [^IIndexWord index-word (locking coarse-lock
-                                                 (.getIndexWord dict stem pos))]
-                  :when index-word]
-              index-word))))
+(defn- word-ids [^IDictionary dict lemma part-of-speech & [stem?]]
+  (let [pos (coerce/pos part-of-speech)]
+    (if stem?
+      (mapcat (memfn ^IIndexWord getWordIDs)
+        (for [stem (stem dict lemma part-of-speech)
+              :let [^IIndexWord index-word (locking coarse-lock (.getIndexWord dict stem pos))]
+              :when index-word]
+          index-word))
+
+      (when-let [index-word (locking coarse-lock (.getIndexWord dict lemma pos))]
+        (.getWordIDs index-word)))))
 
 (defn make-dictionary
   "Initializes a dictionary implementation that mounts files on disk
@@ -63,22 +65,21 @@
       (let [lemma (-> lemma str s/trim)
             part-of-speech (if (empty? part-of-speech) (POS/values) part-of-speech)]
         (when-not (empty? lemma)
-          (if-let [[_ lemma part-of-speech index] (re-matches #"^(.+)#(.)#(\d+)$" lemma)]
+          (if-let [[_ lemma part-of-speech sense] (re-matches #"^(.+)#(.)#(\d+)$" lemma)]
             ; Handle things like "dog#n#1", and return a single instance
-            (nth
-              (fetch-words
-                dict
-                (word-ids dict lemma part-of-speech))
-              (dec (Integer/parseInt index))
-              nil)
+            (first
+              (filter
+                #(= (.getWordNumber ^IWordID (:id %)) (Integer/parseInt sense))
+                (fetch-words dict (word-ids dict lemma part-of-speech false))))
+
             ; else return a list of matches
             (fetch-words dict
               (mapcat
-                (partial word-ids dict lemma)
+                #(word-ids dict lemma % true)
                 part-of-speech))))))))
 
 (defn related-synsets
-  "Use a semantic pointer to fetch related synsets, returning an ordered
+  "Use a semantic pointer to fetch related synsets, returning a
    map of synset-id -> list of words"
   [m & [pointer]]
   (when m
@@ -87,7 +88,7 @@
           related-synsets (if pointer
                             (.getRelatedSynsets (.getSynset word) (coerce/pointer pointer))
                             (.getRelatedSynsets (.getSynset word)))]
-      (apply merge-with (comp vec concat)
+      (apply merge-with concat
         (for [synset-id related-synsets
               word      (.getWords (locking coarse-lock (.getSynset dict synset-id)))]
           {synset-id [(from-java dict word)]})))))
