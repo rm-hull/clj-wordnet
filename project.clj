0cbe457553cdd320f7fe10f0c82e4850e747e115
Thom Lawrence <thom@delver.io>
2014-11-21 16:24:31 +0000

    Bump.

--- project.clj
+++ project.clj
@@ -1,11 +1,10 @@
-(defproject clj-wordnet "0.1.2-SNAPSHOT"
+(defproject clj-wordnet "0.2.0-SNAPSHOT"
   :description "A WordNet/JWI wrapper library"
   :url "https://github.com/delver/clj-wordnet"
   :license {:name "Creative Commons 3.0"
             :url "http://creativecommons.org/licenses/by/3.0/legalcode"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [edu.mit/jwi "2.3.3"]
-                 [org.clojure/data.priority-map "0.0.5"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [edu.mit/jwi "2.3.3"]]
   :source-path "src"
   :min-lein-version "2.3.4"
   :global-vars { *warn-on-reflection* true}
